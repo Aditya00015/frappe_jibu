e732e3d4cbb65982ad5d8bf473f4c9d4d66f203c
aditya <aditya@sanskartechnolab.com>
2024-08-22 12:08:21 +0530

    try exception

--- pos_extension/pos_extension/overrides/insights_settings.py
+++ pos_extension/pos_extension/overrides/insights_settings.py
@@ -1,19 +1,15 @@
 import frappe
+from frappe import _
+
 from insights.insights.doctype.insights_settings.insights_settings import InsightsSettings
 
-def custom_before_save(self):
-    try:
-        # Fetch the existing document from the database if it exists
-        doc_before_save = frappe.get_doc(self.doctype, self.name)
-
-        # If the document is not new and setup_complete is being changed
-        if self.setup_complete and (doc_before_save and not doc_before_save.setup_complete):
-            # Your custom logic here
-            pass
-
-    except Exception as e:
-        frappe.log_error(frappe.get_traceback(), "Custom Insights Settings Error")
-        frappe.throw(_("An error occurred while saving Insights Settings: {0}").format(str(e)))
-
-# Override the before_save method
-InsightsSettings.before_save = custom_before_save
+class CustomInsightsSettings(InsightsSettings):
+    def before_save(self):
+        try:
+            # Custom logic before save
+            doc_before_save = frappe.get_doc(self.doctype, self.name)
+            if self.setup_complete and (doc_before_save and not doc_before_save.setup_complete):
+                pass
+        except Exception as e:
+            frappe.log_error(frappe.get_traceback(), "Custom Insights Settings Error")
+            frappe.throw(_("An error occurred while saving Insights Settings: {0}").format(str(e)))
